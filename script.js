671741bbc68415263ccdb65d67f2cf9757f57c96
Gerdeth <gerdethwickham@gmail.com>
2021-10-11 12:57:52 -0500

    fixed display

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 (function(){
     var questions =[{
-        question: "2*9=",
+        question: "2*9 =",
         choices:[2,7,11,18],
         correctAnswer:18
     },{
-        question: "What is 8*9?",
+        question: "What is 8*9 ?",
         choices: [72, 99, 108, 134, 156],
         correctAnswer: 72
     },{
-        question: "What is the product of 6 and 9?",
+        question: "What is the product of 6 and 9 ?",
         choices: [12, 19, 54, 134, 146],
         correctAnswer: 54
     },{
@@ -16,7 +16,7 @@
         choices: [26, 108, 134, 153,963],
         correctAnswer: 153
     },{
-        question: "What is 149*9?",
+        question: "What is 149*9 ?",
         choices: [ 158, 1341,1091, 1561],
         correctAnswer: 1341
     }
