675f553d67b56e4b971a62923ee8e1ef54a7fbda
Gerdeth <gerdethwickham@gmail.com>
2021-10-11 10:37:21 -0500

    add questions

--- script.js
+++ script.js
@@ -6,6 +6,18 @@
     },{
         question: "What is 8*9?",
         choices: [72, 99, 108, 134, 156],
-        correctAnswer: 0
+        correctAnswer: 72
+    },{
+        question: "What is the product of 6 and 9?",
+        choices: [12, 19, 54, 134, 146],
+        correctAnswer: 54
+    },{
+        question: "What is 17 multiplied by 9?",
+        choices: [26, 108, 134, 153,963],
+        correctAnswer: 153
+    },{
+        question: "What is 149*9?",
+        choices: [ 158, 1341,1091, 1561],
+        correctAnswer: 1341
     }
     ]})
