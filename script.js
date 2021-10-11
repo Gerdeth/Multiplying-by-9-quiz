aad9a9536dc852ccd0de9654442d495f426d73da
Gerdeth <gerdethwickham@gmail.com>
2021-10-10 20:54:04 -0500

    add more questions

--- script.js
+++ script.js
@@ -3,5 +3,9 @@
         question: "2*9=",
         choices:[2,7,11,18],
         correctAnswer:18
-    }]
-})
+    },{
+        question: "What is 8*9?",
+        choices: [72, 99, 108, 134, 156],
+        correctAnswer: 0
+    }
+    ]})
