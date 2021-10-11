7d008ac98972068691a81e68af29ce8d9deefe3f
Gerdeth <gerdethwickham@gmail.com>
2021-10-11 11:48:18 -0500

    event handlers

--- script.js
+++ script.js
@@ -21,3 +21,66 @@
         correctAnswer: 1341
     }
     ]})
+
+    var questionCounter = 0; // Tracks question number
+    var selections =[]; //Contains user choices
+    var quiz = $('#quiz'); //quiz div object
+
+    displayNext(); //Display initial question
+   
+  $('#next').on('click', function (e) {
+    e.preventDefault();  // Click handler for the 'next' button
+    
+    
+    if(quiz.is(':animated')) {        
+      return false;
+    }
+    choose(); // Suspend click listener during fade animation
+    
+    // If no user selection, progress is stopped
+    if (isNaN(selections[questionCounter])) {
+      alert('Please make a selection!');
+    } else {
+      questionCounter++;
+      displayNext();
+    }
+  });
+  
+  // Click handler for the 'prev' button
+  $('#prev').on('click', function (e) {
+    e.preventDefault();
+    
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    choose();
+    questionCounter--;
+    displayNext();
+  });
+  
+  // Click handler for the 'Start Over' button
+  $('#start').on('click', function (e) {
+    e.preventDefault();
+    
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    questionCounter = 0;
+    selections = [];
+    displayNext();
+    $('#start').hide();
+  });
+
+    function displayNext(){
+        quiz.fadeOut(function(){
+            $('#question').remove();
+
+            if(questionCounter<questions.length){
+                var nextQuestion= createQuestionElement(questionCounter);
+                quiz.append(nextQuestion).fadeIn();
+                if (!(isNaN(selections[questionCounter]))){
+                    $('input[value='+selections[questionCounter]+']').prop('checked',true);
+                }
+            }
+        })
+    }
